4a470ffe400b6da1e935086a35f0a02e8c32705d
karananjan2002 <133091705+karananjan2002@users.noreply.github.com>
2024-02-18 08:48:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all the code written in the file as newer versions
+
+// alert(3 + 3) //we are not using like  this  as we are using nodejs not as document or browser
+
+//code readability should be high
+
+
+let name = "Karan"
+let age = 21
+let isLoggedin = false
+
+//Primitive datatypes
+
+//number => 2 power 0 to 2 power 53
+//bigint
+//string
+//boolean => true/false
+//null  => standalone value 
+//undefined => no value assigned
+//symbol
+
+
+//object
+console.log(typeof name);
